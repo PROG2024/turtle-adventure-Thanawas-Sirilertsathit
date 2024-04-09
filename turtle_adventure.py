d666b7307e692a1f01aa99e85b8580898436dea4
Thanawas-Sirilertsathit <thanawas.s@ku.th>
2024-04-09 21:06:28 +0700

    added trapper enemy
    - trapper enemies and traps are added
    - summoner enemies shoot bullets periodically

--- turtle_adventure.py
+++ turtle_adventure.py
@@ -248,8 +248,10 @@ class Enemy(TurtleGameElement):
 # Random walking Enemy walk randomly
 # Chasing Enemy pursuit the turtle
 # Fencing Enemy blocking around the home in a square movement
-# Summon Enemy shoot scatter shot when summon
-# Scatter bullets accelerate itself and bounce the wall
+# Summon Enemy and Boss Enemy shoot scatter shot periodically
+# Scatter bullets accelerate itself and bounce the wall (unpredictable movement)
+# Trapper Enemy deploys traps periodically
+# Traps stay still but if turtle comes in and hit = die
 class ShakingEnemy(Enemy):
     """
     Random movement and has acceleration
@@ -579,6 +581,123 @@ class BossEnemy(Enemy):
 
     def delete(self):
         pass
+
+class Trapper1Enemy(Enemy):
+    """
+    Create traps along its path
+    """
+
+    def __init__(self,
+                 game: "TurtleAdventureGame",
+                 size: int,
+                 color: str,
+                 ):
+        super().__init__(game, size, color)
+        self.__id = None
+        self.speed = math.ceil(self.game.level/2)
+        self.choosing = random.choice(["Up","Down","Right","Left"])
+
+    def create(self):
+        self.__id = self.canvas.create_oval(self.x-self.size/2,self.y-self.size/2,self.x+self.size/2,self.y+self.size/2, fill = self.color)
+
+    def update(self):
+        if self.choosing == "Right":
+            self.x += math.floor(self.speed)
+        elif self.choosing == "Left":
+            self.x -= math.floor(self.speed)
+        elif self.choosing == "Up":
+            self.y += math.floor(self.speed)
+        else:
+            self.y -= math.floor(self.speed)
+        if self.hits_player():
+            self.game.game_over_lose()
+        if self.x >= 800:
+            self.speed = self.speed*-1
+        elif self.x <= 0:
+            self.speed = self.speed*-1
+        if self.y >= 500:
+            self.speed = self.speed*-1
+        elif self.y <= 0:
+            self.speed = self.speed*-1
+
+    def render(self):
+        self.canvas.coords(self.__id, self.x-self.size/2, self.y-self.size/2, self.x+self.size/2, self.y+self.size/2)
+
+    def delete(self):
+        pass
+
+class Trapper2Enemy(Enemy):
+    """
+    Create traps along its path
+    """
+
+    def __init__(self,
+                 game: "TurtleAdventureGame",
+                 size: int,
+                 color: str,
+                 ):
+        super().__init__(game, size, color)
+        self.__id = None
+        self.speed = math.ceil(self.game.level/2)
+        self.choosing1 = random.choice(["Up","Down"])
+        self.choosing2 = random.choice(["Right","Left"])
+
+    def create(self):
+        self.__id = self.canvas.create_oval(self.x-self.size/2,self.y-self.size/2,self.x+self.size/2,self.y+self.size/2, fill = self.color)
+
+    def update(self):
+        if self.choosing1 == "Up":
+            self.y += self.speed
+        else:
+            self.y -= self.speed
+        if self.choosing2 == "Right":
+            self.x += self.speed
+        else:
+            self.x -= self.speed
+        if self.hits_player():
+            self.game.game_over_lose()
+        if self.x >= 800:
+            self.speed = self.speed*-1
+        elif self.x <= 0:
+            self.speed = self.speed*-1
+        if self.y >= 500:
+            self.speed = self.speed*-1
+        elif self.y <= 0:
+            self.speed = self.speed*-1
+
+    def render(self):
+        self.canvas.coords(self.__id, self.x-self.size/2, self.y-self.size/2, self.x+self.size/2, self.y+self.size/2)
+
+    def delete(self):
+        pass
+
+class TrapEnemy(Enemy):
+    """
+    It is a trap!
+    """
+
+    def __init__(self,
+                 game: "TurtleAdventureGame",
+                 size: int,
+                 color: str,
+                 ):
+        super().__init__(game, size, color)
+        self.__id = None
+        self.speed = 2
+        self.choosing = random.choice(["Up","Down","Right","Left"])
+
+    def create(self):
+        self.__id = self.canvas.create_rectangle(self.x-self.size/2,self.y-self.size/2,self.x+self.size/2,self.y+self.size/2, fill = self.color)
+
+    def update(self):
+        if self.hits_player():
+            self.game.game_over_lose()
+
+    def render(self):
+        self.canvas.coords(self.__id, self.x-self.size/2, self.y-self.size/2, self.x+self.size/2, self.y+self.size/2)
+
+    def delete(self):
+        pass
     
 class EnemyGenerator:
     """
@@ -623,36 +742,55 @@ class EnemyGenerator:
         new_enemy_a.x = self.__game.home.x - 50
         new_enemy_a.y = self.__game.home.y - 50
         self.game.add_element(new_enemy_a)            
-        new_enemy_s = SummonEnemy(self.__game, 20, "green")
+        new_enemy_s = SummonEnemy(self.__game, 30, "green")
         new_enemy_s.x = random.randint(200,800)
         new_enemy_s.y = random.randint(0,400)
         self.game.add_element(new_enemy_s)
-        new_enemy_b = BossEnemy(self.__game, 20, "orange")
+        new_enemy_b = BossEnemy(self.__game, 30, "orange")
         new_enemy_b.x = random.randint(200,800)
         new_enemy_b.y = random.randint(0,400)
         self.game.add_element(new_enemy_b)
-        for i in range(math.ceil((self.level/3)+2)):
-            self.create_summon(new_enemy_s.x, new_enemy_s.y, new_enemy_s)
-        for i in range(math.ceil(self.level/3)+2):
-            self.create_boss(new_enemy_b.x, new_enemy_b.y, new_enemy_b)        
+        new_enemy_t1 = Trapper1Enemy(self.__game, 30, "magenta")
+        new_enemy_t1.x = random.randint(200,800)
+        new_enemy_t1.y = random.randint(0,400)
+        self.game.add_element(new_enemy_t1)
+        new_enemy_t2 = Trapper2Enemy(self.__game, 30, "gold")
+        new_enemy_t2.x = random.randint(200,800)
+        new_enemy_t2.y = random.randint(0,400)
+        self.game.add_element(new_enemy_t2)
+        for i in range(3):
+            self.create_summon(new_enemy_s)
+        for i in range(3):
+            self.create_boss(new_enemy_b)
+        self.create_trapper(new_enemy_t1)        
+        self.create_trapper(new_enemy_t2)        
         self.__game.after(math.ceil((10000/self.level)+1500), self.create_enemy)
 
-    def create_summon(self,x,y, summoner):
+    def create_summon(self,summoner):
         new_enemy_dm = DiagonalMinionEnemy(self.__game, 10, "pink", summoner)
-        new_enemy_dm.x = random.randint(x-10,x+10)
-        new_enemy_dm.y = random.randint(y-10,y+10)
+        new_enemy_dm.x = random.randint(summoner.x-10,summoner.x+10)
+        new_enemy_dm.y = random.randint(summoner.y-10,summoner.y+10)
         self.game.add_element(new_enemy_dm)
         # new_enemy_sm = StraightMinionEnemy(self.__game, 20, "pink", summoner)
         # new_enemy_sm.x = random.randint(x-10,x+10)
         # new_enemy_sm.y = random.randint(y-10,y+10)
         # self.game.add_element(new_enemy_sm)
+        self.__game.after(math.ceil(10000/self.level)+100, lambda: self.create_summon(summoner))
     
-    def create_boss(self,x,y,boss):
-        new_enemy_sm = StraightMinionEnemy(self.__game, 10, "pink", boss)
-        new_enemy_sm.x = random.randint(x-10,x+10)
-        new_enemy_sm.y = random.randint(y-10,y+10)
+    def create_boss(self,boss):
+        new_enemy_sm = StraightMinionEnemy(self.__game, 10, "linen", boss)
+        new_enemy_sm.x = random.randint(boss.x-10,boss.x+10)
+        new_enemy_sm.y = random.randint(boss.y-10,boss.y+10)
         self.game.add_element(new_enemy_sm)
+        self.__game.after(math.ceil(10000/self.level)+100, lambda: self.create_boss(boss))
 
+    def create_trapper(self,trapper):
+        new_enemy_trap = TrapEnemy(self.__game, 20, "gray")
+        new_enemy_trap.x = random.randint(trapper.x-5,trapper.x+5)
+        new_enemy_trap.y = random.randint(trapper.y-5,trapper.y+5)
+        self.game.add_element(new_enemy_trap)
+        self.__game.after(math.ceil(10000/self.level)+100, lambda: self.create_trapper(trapper))
+        # self.__game.after(100, lambda: self.create_trapper(trapper))
 
 class TurtleAdventureGame(Game): # pylint: disable=too-many-ancestors
     """
